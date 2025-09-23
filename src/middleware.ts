b094a3ee8dc4c8b20d02b395a5305a14dc3566ba
nebulacommander <godwinonyerikam@gmail.com>
2025-09-23 15:36:07 +0100

    add Convex auth Next.js middleware

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { convexAuthNextjsMiddleware, createRouteMatcher, nextjsMiddlewareRedirect } from '@convex-dev/auth/nextjs/server'
+import { isBypassRoutes, isProtectedRoutes, isPublicRoutes } from './lib/permissions'
+
+const PublicMatcher = createRouteMatcher(isPublicRoutes)
+const ProtectedMatcher = createRouteMatcher(isProtectedRoutes)
+const BypassMatcher = createRouteMatcher(isBypassRoutes);
+
+export default convexAuthNextjsMiddleware(async (request, { convexAuth }) => {
+  if (BypassMatcher(request)) return
+  const authed = await convexAuth.isAuthenticated()
+  if (PublicMatcher(request) && authed) {
+    return nextjsMiddlewareRedirect(request, `/dashboard`)
+  }
+  if (ProtectedMatcher(request) && !authed) {
+    return nextjsMiddlewareRedirect(request, `/auth/sign-in`)
+  }
+  return
+},
+  {
+    cookieConfig: { maxAge: 60 * 60 * 24 * 30 } // 30 days
+  }
+
+)
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+}
