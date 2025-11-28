689634705fc1bb8571fef677ba35c6a05b59e0d9
nebulacommander <godwinonyerikam@gmail.com>
2025-11-28 05:37:22 +0100

    feat: add style guide redux api

---
+++ src/redux/api/style-guide/index.ts
@@ -0,0 +1,41 @@
+export interface ColorSection {
+    title:
+      | 'Primary Colours'
+      | 'Secondary & Accent Colours'
+      | 'Utility & Form Colours'
+      | 'Status & Feedback Colours'
+    swatches: ColorSwatch[]
+}
+
+export interface ColorSwatch {
+    name: string
+    hexColor: string
+    description?: string
+}
+
+export interface TypographyStyle {
+    name: string
+    fontFamily: string
+    fontSize: string
+    fontWeight: string
+    lineHeight: string
+    letterSpacing?: string
+    description?: string
+}
+
+export interface TypographySection {
+    title: string
+    styles: TypographyStyle[]
+}
+
+export interface StyleGuide {
+    theme: string
+    description: string
+    colorSections: [
+        ColorSection,
+        ColorSection,
+        ColorSection,
+        ColorSection,
+    ]
+    typographySections: [TypographySection, TypographySection, TypographySection]
+}
